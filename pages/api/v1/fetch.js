674f5d9563a28dd0159d7d6f214c83e7b3e4d05b
Nikhil Mungel <nikhil.mungel@substackinc.com>
2022-10-30 00:12:28 -0700

    almost ready

--- pages/api/v1/fetch.js
+++ pages/api/v1/fetch.js
@@ -4,14 +4,17 @@ export default async function handler(req, res) {
     const val = req.query["url"];
     console.log("req url: " + val);
 
+    const d1 = new Date().getTime();
     const response = await fetch(val);
     const data = await response;
+    const d2 = new Date().getTime() - d1;
 
-    let resp = {}
+    let headers = {}
     for (var pair of data.headers.entries()) {
         console.log(pair[0]+ ': '+ pair[1]);
-        resp[pair[0]] = pair[1];
+        headers[pair[0]] = pair[1];
     }
 
-    res.status(200).json({ name: 'John Doe', resp: resp });
+    res.status(200).json({ name: 'John Doe', latency: d2, status: response.status + " " + response.statusText, headers: headers });
+
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { Welcome } from '../components/Welcome/Welcome';
 import { useState, useRef, SetStateAction} from 'react';
 
-import {AppShell, Navbar, Header, Autocomplete, Loader, Button, Collapse, Table} from '@mantine/core';
+import {AppShell, Navbar, Header, Autocomplete, Loader, Button, Collapse, Table, Grid, NativeSelect} from '@mantine/core';
 
 import {ColorSchemeToggle} from '../components/ColorSchemeToggle/ColorSchemeToggle';
 
@@ -9,25 +9,36 @@ export default function HomePage() {
 
     const timeoutRef = useRef<number>(-1);
     const [value, setValue] = useState('');
+    const [methodValue, setMethodValue] = useState('');
     const [loading, setLoading] = useState(false);
     const [data, setData] = useState<string[]>([]);
     const [response, setResponse] = useState<string[]>([]);
     const [opened, setOpened] = useState(false);
     const [rows, setRows] = useState<object[]>([]);
 
+    function isValidHttpUrl(string) {
+        let url;
+        try {
+            url = new URL(string);
+        } catch (_) {
+            return false;
+        }
+        return url.protocol === "http:" || url.protocol === "https:";
+    }
+
     const handleChange = (val: string) => {
         window.clearTimeout(timeoutRef.current);
         setValue(val);
         setData([]);
 
-        if (val.trim().length !== 0) {
+        if (isValidHttpUrl(val)) {
             fetch(`/api/v1/fetch?url=${encodeURIComponent(val)}`)
                 .then(response => response.json())
                 .then(data => {
                     setResponse(data);
                     console.log(data);
                     let buffer: SetStateAction<object[]> = [];
-                    let dd = new Map(Object.entries(data.resp));
+                    let dd = new Map(Object.entries(data.headers));
                     dd.forEach(function(value, key) {
                         // @ts-ignore
                         buffer.push(<tr><td>{key}</td><td>{value}</td></tr>);
@@ -50,33 +61,47 @@ export default function HomePage() {
                 main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
             })}
         >
-            <ColorSchemeToggle />
-            <Autocomplete
-                value={value}
-                data={data}
-                onChange={handleChange}
-                rightSection={loading ? <Loader size={16} /> : null}
-                label="URL"
-                placeholder="https://blog.httpcolon.dev/"
-            />
-            {!loading &&
-                <div>
-                    <Table>
-                        <thead>
-                        <tr>
-                            <th>Response Header</th>
-                            <th>Value</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                          {rows}
-                        </tbody>
-                    </Table>
-                    {console.log(response)}
-                    {console.log(rows)}
-                </div>
 
-            }
+            <Grid>
+                <Grid.Col span={"content"}>
+                    <NativeSelect
+                        data={['GET', 'POST', 'PUT', 'DELETE']}
+                        value={methodValue}
+                        onChange={handleChange}
+                        placeholder="GET"
+                    />
+                </Grid.Col>
+
+                <Grid.Col span={"auto"}>
+                    <Autocomplete
+                    value={value}
+                    data={data}
+                    onChange={handleChange}
+                    rightSection={loading ? <Loader size={16} /> : null}
+                    placeholder="https://blog.httpcolon.dev/"/>
+                </Grid.Col>
+            </Grid>
+
+
+            <Grid>
+                <Grid.Col span={4}>Status: {response.status}</Grid.Col>
+                <Grid.Col span={4}>Latency: {response.latency} ms</Grid.Col>
+                <Grid.Col span={4}>Size: </Grid.Col>
+            </Grid>
+
+
+                <Table>
+                    <thead>
+                    <tr>
+                        <th>Response Header</th>
+                        <th>Value</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                      {rows}
+                    </tbody>
+                </Table>
+
         </AppShell>
     </>
   );

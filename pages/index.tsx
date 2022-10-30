a8c70cb922cebc94533163776cb5d5d350c4f363
Nikhil Mungel <nikhil.mungel@substackinc.com>
2022-10-30 00:14:45 -0700

    almost ready 3

--- pages/index.tsx
+++ pages/index.tsx
@@ -67,7 +67,6 @@ export default function HomePage() {
                     <NativeSelect
                         data={['GET', 'POST', 'PUT', 'DELETE']}
                         value={methodValue}
-                        onChange={handleChange}
                         placeholder="GET"
                     />
                 </Grid.Col>
